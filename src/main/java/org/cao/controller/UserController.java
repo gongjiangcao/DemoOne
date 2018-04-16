213d4ea076ff10d500851a8d5cb2b70c26abd764
gongjiangcao <jingcao268>
2018-04-16 22:24:27 +0800

    注释了两个方法

--- src/main/java/org/cao/controller/UserController.java
+++ src/main/java/org/cao/controller/UserController.java
@@ -41,10 +41,10 @@ public class UserController {
 		System.out.println("我修改了");
 	}
 
-	public   void eat(){
-        System.out.println("我吃了");
-    }
-	public   void sleep(){
-        System.out.println("我睡了");
-    }
+//	public   void eat(){
+//        System.out.println("我吃了");
+//    }
+//	public   void sleep(){
+//        System.out.println("我睡了");
+//    }
 }
